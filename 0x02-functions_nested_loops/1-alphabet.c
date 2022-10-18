b90b6e05e786f06daa56d85cbf5141c786124d83
Godielesh <godielesh@gmail.com>
2022-10-18 08:16:51 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - Lowercase alphabets
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'Z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+	return (0);
+}
