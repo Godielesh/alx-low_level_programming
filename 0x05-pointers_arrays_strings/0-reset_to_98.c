aea8528b94d5f4914b74fcca7e179c2c3c8f6279
Godielesh <godielesh@gmail.com>
2022-10-25 06:07:29 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - pointer and update it to 98
+ * @n: a pointer
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * prototype files
+ * import forms
+ */
+
+void reset_to_98(int *n);
+
+#endif
