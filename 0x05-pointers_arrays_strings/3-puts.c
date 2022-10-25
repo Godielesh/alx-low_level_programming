83216e457b6778975fba7bb542c414967e839158
Godielesh <godielesh@gmail.com>
2022-10-25 08:17:00 -0700

    Task 3. Don't fear

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  * _puts - it puts
- * @str: _puts function
+ * @str: a param _puts function
  */
 void _puts(char *str)
 {
-	for (; *str != '\0'; *str++)
+	for (; *str != '\0'; str++)
 	{
 		_putchar(*str);
 	}
